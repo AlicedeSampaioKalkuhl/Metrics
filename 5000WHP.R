025fe5524182601f7d3eb8fa6e6e1ef334f317c8
AlicedeSampaioKalkuhl <alicekalkuhl@freenet.de>
2021-11-02 08:41:11 +0000

    01.11.2021

--- 5000WHP.R
+++ 5000WHP.R
@@ -40,33 +40,15 @@ progress <- ggplot(Progress, aes(x=Date, y= WPH, fill=WPH))+
 
 consistency<-
 ggplot(Consistency, aes(x=Date, y= WPD, fill=WPD))+
-  geom_hline(yintercept=425, colour="blue")+
+  geom_hline(yintercept=25, colour="blue")+
   geom_bar(stat="identity", width=1)+
   scale_fill_viridis(option="plasma",direction=-1)+
   ylab("daily words")+
   theme(axis.text.x=element_text(angle=60,hjust=1))+theme_bw()
 
 png("daily.png")
-  plot_grid(progress+theme(legend.position="none"),consistency+theme(legend.position="none"))
+consistency+theme(legend.position="none")
 dev.off()
-
-monthly<-
-plot_grid(
-  ggplot(Averages, aes(x=month, y= WPH, fill=WPH))+
-    geom_bar(stat="identity", width=1)+
-    scale_fill_viridis(direction=-1)+
-    theme(axis.text.x=element_text(angle=60,hjust=1))+
-    theme(legend.position="none")+
-    facet_wrap(~year),
-  ggplot(Averages, aes(x=month, y= WPD, fill=WPD))+
-    geom_bar(stat="identity", width=1)+
-    scale_fill_viridis(direction=-1)+
-    theme(axis.text.x=element_text(angle=60,hjust=1))+
-    theme(legend.position="none")+
-    facet_wrap(~year)
-)
-
-
 
 WriMos<-ggplot(WriMo,aes(x=Date,y=monthly,fill=monthly))+
   geom_bar(stat="identity")+
@@ -74,23 +56,11 @@ WriMos<-ggplot(WriMo,aes(x=Date,y=monthly,fill=monthly))+
   ylab("progress")+
   theme_bw()
 
-#2021
-JuNoWriMo<-WriMo%>% 
-  filter(month=="06")%>%
-  complete(Date=seq.Date(min(Date),as.Date("2021-06-30"),by ="day")) %>% 
-  mutate(year=format(Date,"%Y")) %>% group_by(year) %>% mutate(goal=seq(from=1667,to=50000,length.out=30)) 
-JuNoWriMo%>%
-  ggplot(aes(Date))+
-    geom_bar(aes(y=goal,fill=goal),stat="identity",position="dodge",width=1,alpha=0.5)+
-    geom_bar(aes(y=monthly,fill=monthly),stat="identity",position="dodge",width=1)+
-    scale_fill_viridis(option="turbo")+
-    labs(fill="Words")+ylab("Words")
-
-August<-WriMo%>%
-  filter(month=="08")%>%
-  complete(Date=seq.Date(min(Date),as.Date("2021-08-31"),by="day"))%>% 
+October<-WriMo%>%
+  filter(month=="10")%>%
+  complete(Date=seq.Date(min(Date),as.Date("2021-10-31"),by="day"))%>% 
   mutate(year=format(Date,"%Y"))%>%group_by(year)%>%mutate(goal=seq(from=1667,to=50000,length.out=31)) 
-August%>%
+October%>%
   ggplot(aes(Date))+
   geom_bar(aes(y=goal,fill=goal),stat="identity",position="dodge",width=1,alpha=0.5)+
   geom_bar(aes(y=monthly,fill=monthly),stat="identity",position="dodge",width=1)+
@@ -100,5 +70,6 @@ August%>%
 
 NaNoWriMo <- data.frame(goal=seq(from=1667,to=50000,by=1667))
 
-View(August)
-#load.image("daily.png")%>%plot(axes=F)
+View(October)
+View(Averages)
+load.image("daily.png")%>%plot(axes=F)
